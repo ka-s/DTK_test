9a868a008b18421ed29ad302c38fcc0be2eb80bc
ka-s <using0namespace0kas@gmail.com>
2015-05-20 16:00:46 +0900

    FirstCommit

---
+++ RenderManager.cpp
@@ -0,0 +1,14 @@
+// 
+// RenderManager.cpp
+// 
+
+// @Author ka-s
+
+#include "pch.h"
+#include "RenderManager.h"
+
+// •`‰æ
+void render_Manager::render()
+{
+
+}
---
+++ RenderManager.h
@@ -0,0 +1,18 @@
+//
+// RenderManager.h
+//
+
+// @Author ka-s
+
+#pragma once
+
+#include "pch.h"
+
+class render_Manager
+{
+private:
+
+public:
+    // •`‰æ
+    void render();
+};
