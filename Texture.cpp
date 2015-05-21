b001846b07a9b4932d172026a0d40818edca7b95
ka-s_mac <using0namespace0kas@gmail.com>
2015-05-21 11:03:29 +0900

    TextureClass追加

--- Texture.cpp
+++ Texture.cpp
@@ -7,34 +7,15 @@
 #include "pch.h"
 #include "Texture.h"
 
-using namespace DirectX;
-using namespace DirectX::SimpleMath;
-using Microsoft::WRL::ComPtr;
-
 // コンストラクタ
-//   画像読み込み
+//   テクスチャパスを渡す
 texture::texture(std::wstring pass)
 {
-    // SpriteBatchリセット
-    m_spriteBatch.reset(new SpriteBatch(d3dContext.Get()));
-    // リソース変数作成
-    ComPtr<ID3D11Resource> resource;
 
-    // テクスチャ読み込み
-    DX::ThrowIfFailed(
-        CreateWICTextureFromFile(d3dDevice.Get(), pass, resource.GetAddressOf(),
-        m_texture.ReleaseAndGetAddressOf()));
 }
 
 // 描画メソッド
-void texture::render(DirectX::SimpleMath::Vector2 pos)
+void texture::render()
 {
-    // SpriteBatch描画開始
-    m_spriteBatch->Begin();
 
-    // 猫描画
-    m_spriteBatch->Draw(m_texture.Get(), pos, nullptr, Colors::White);
-
-    // SpriteBatch描画終了
-    m_spriteBatch->End();
 }
--- Texture.h
+++ Texture.h
@@ -11,20 +11,12 @@
 class texture
 {
 private:
-    // テクスチャ
-    Microsoft::WRL::ComPtr<ID3D11ShaderResourceView> m_texture;
-    // スプライトバッチ
-    std::unique_ptr<DirectX::SpriteBatch> m_spriteBatch;
-
-    // Direct3Dのオブジェクト
-    Microsoft::WRL::ComPtr<ID3D11Device>            d3dDevice;
-    Microsoft::WRL::ComPtr<ID3D11DeviceContext>     d3dContext;
 
 public:
     // コンストラクタ
-    //   画像読み込み
+    //   テクスチャパスを渡す
     texture(std::wstring pass);
 
     // 描画メソッド
-    void render(DirectX::SimpleMath::Vector2 pos);
+    void render();
 };
